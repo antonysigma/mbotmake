4487de8998367bb82a2407fb187639737a53db67
Antony Chan <antony.sigma@gmail.com>
2025-04-06 14:39:29 -0700

    Skip thumbnail generations

--- mbotmake2/extract_thumbnails.py
+++ mbotmake2/extract_thumbnails.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 
+from parsimonious.exceptions import ParseError
+
 from mbotmake2.grammars.thumbnails import grammar
 from mbotmake2.transformers.thumbnails import PNGImage, ThumbnailDecoder
 
 
 def extractThumbnails(filename: Path, output_dir: Path) -> list[Path]:
     with open(filename, "r") as file:
-        ast = grammar.match(file.read(5_000_000))
+        try:
+            ast = grammar.match(file.read(5_000_000))
+        except ParseError:
+            print("Thumbnail not found. Skipping thumbnail generation...")
+            return []
 
     decoder = ThumbnailDecoder()
     thumbnails: list[PNGImage] = decoder.visit(ast)
